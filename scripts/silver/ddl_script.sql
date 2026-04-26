731a30c07f30393428f509f55abf8f99a4eb4abd
shreyasri1108 <97795669+shreyasri1108@users.noreply.github.com>
2026-04-26 12:51:41 +0530

    Create ddl_script.sql

---
+++ scripts/silver/ddl_script.sql
@@ -0,0 +1,99 @@
+USE datawarehouse
+
+--creating a DDL command
+-- for cust_info (FIRST CHECKS IF TABLE WITH SAME OBJECT EXISTS)
+IF OBJECT_ID ('Silver.crm_cust_info' , 'U') IS NOT NULL
+     DROP TABLE Silver.crm_cust_info;
+CREATE TABLE Silver.crm_cust_info
+(
+cst_id INT ,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_marital_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
+cst_create_date DATE,
+dwh_create_date DATETIME2 default GETDATE()    -- THIS VALUE COMES FROM ENGINEER SIDE , TO GET DEBUGGING DETAILS DURING ISSUE
+);
+
+IF OBJECT_ID ('Silver.crm_prd_info' , 'U') IS NOT NULL
+     DROP TABLE Silver.crm_prd_info;
+CREATE TABLE Silver.crm_prd_info
+(
+prd_id INT ,
+cat_id NVARCHAR(50),
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(100),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATE,
+prd_end_dt DATE,
+dwh_create_date DATETIME2 default GETDATE()    -- THIS VALUE COMES FROM ENGINEER SIDE , TO GET DEBUGGING DETAILS DURING ISSUE
+);
+
+-- TO CHANGE COLUMN DATATYPES FOR MULTIPLE VALUES 
+/*
+ALTER TABLE Bronze.crm_prd_info
+ALTER COLUMN prd_line NVARCHAR(50)
+GO
+ALTER TABLE Bronze.crm_prd_info
+ALTER COLUMN prd_start_dt DATETIME
+GO
+ALTER TABLE Bronze.crm_prd_info
+ALTER COLUMN prd_end_dt DATETIME
+*/
+
+IF OBJECT_ID ('Silver.crm_sales_details' , 'U') IS NOT NULL
+     DROP TABLE Silver.crm_sales_details;
+CREATE TABLE Silver.crm_sales_details
+(
+sls_ord_num NVARCHAR(50) ,
+sls_prd_key NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt DATE,
+sls_ship_dt DATE,
+sls_due_dt DATE,
+sls_sales INT,
+sls_quantity INT ,
+sls_price INT,
+dwh_create_date DATETIME2 default GETDATE()    -- THIS VALUE COMES FROM ENGINEER SIDE , TO GET DEBUGGING DETAILS DURING ISSUE
+);
+
+/*
+ALTER TABLE Bronze.crm_sales_details
+ADD  sls_order_dt INT
+GO
+ALTER TABLE Bronze.crm_sales_details
+ADD  sls_ship_dt INT
+GO
+*/
+
+IF OBJECT_ID ('Silver.erp_cust_az12' , 'U') IS NOT NULL
+     DROP TABLE Silver.erp_cust_az12;
+CREATE TABLE Silver.erp_cust_az12
+(
+CID NVARCHAR(50) ,
+BDATE DATE,
+GEN NVARCHAR(10),
+dwh_create_date DATETIME2 default GETDATE()    -- THIS VALUE COMES FROM ENGINEER SIDE , TO GET DEBUGGING DETAILS DURING ISSUE
+);
+
+IF OBJECT_ID ('Silver.erp_loc_a101' , 'U') IS NOT NULL
+     DROP TABLE Silver.erp_loc_a101;
+CREATE TABLE Silver.erp_loc_a101
+(
+CID NVARCHAR(50) ,
+CNTRY NVARCHAR(50),
+dwh_create_date DATETIME2 default GETDATE()    -- THIS VALUE COMES FROM ENGINEER SIDE , TO GET DEBUGGING DETAILS DURING ISSUE
+);
+
+IF OBJECT_ID ('Silver.erp_px_cat_g1v2' , 'U') IS NOT NULL
+     DROP TABLE Silver.erp_px_cat_g1v2;
+CREATE TABLE Silver.erp_px_cat_g1v2
+(
+ID NVARCHAR(50),
+CAT NVARCHAR(50) ,
+SUNCAT NVARCHAR(50),
+MAINTENANCE NVARCHAR(3),
+dwh_create_date DATETIME2 default GETDATE()    -- THIS VALUE COMES FROM ENGINEER SIDE , TO GET DEBUGGING DETAILS DURING ISSUE
+);
